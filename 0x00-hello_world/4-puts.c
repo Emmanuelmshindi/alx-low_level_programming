c9424481997145593b2e0d8c9fb79dcdf4a953f1
Emmanuelmshindi <mshindimanuel@gmail.com>
2022-07-03 13:37:56 -0700

    Hello world task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-#include <studio.h>
-
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
