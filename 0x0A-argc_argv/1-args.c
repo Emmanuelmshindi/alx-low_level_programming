08b0f70915f7fe526962f0d9128f85663d4995bd
Emmanuelmshindi <mshindimanuel@gmail.com>
2022-11-11 13:52:25 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,16 +9,6 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-int i;
-
-if (argc == 1)
 printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++)
-
-printf("%d\n", i - 1);
-}
-
 return (0);
 }
