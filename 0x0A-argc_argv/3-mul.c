8bcae6bf4702eefb60f3f6e60fba2f379d547e5d
Emmanuelmshindi <mshindimanuel@gmail.com>
2022-11-11 22:27:45 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,22 @@
  */
 int main(int argc, char *argv[])
 {
-int result, num1, num2;
+int i, j;
 
-if (argc != 3)
+if (argc == 1 || argc == 2)
 {
-printf("%s\n", "Error");
+printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
+else
+{
+j = i;
 
-printf("%d\n", result);
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+
+printf("%d\n", j);
+}
+
 return (0);
 }
