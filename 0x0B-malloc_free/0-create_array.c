09f7ba6f27cb72c9113f867a473bece50d8336e7
Emmanuelmshindi <mshindimanuel@gmail.com>
2022-11-14 09:51:17 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,15 +10,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *str;
-unsigned int i;
+char *array;
+unsigned int index;
 
-str = malloc(sizeof(char) * size);
-if (size == 0 || str == NULL)
+if (size == 0)
 return (NULL);
 
-for (i = 0; i < size; i++)
-str[i] = c;
+array = malloc(sizeof(char) * size);
 
-return (str);
+if (array == NULL)
+return (NULL);
+
+for (index = 0; index < size; index++)
+array[index] = c;
+
+return (array);
 }
